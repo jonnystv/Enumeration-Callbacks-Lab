bbb8983a1118ba24f2b86e6c52fab3d867f8562c
Ewan Brain <ewan.brain@gmail.com>
2021-01-07 15:26:43 +0000

    MVP almost done

--- models/cinema.js
+++ models/cinema.js
@@ -16,4 +16,25 @@ Cinema.prototype.findFilmByTitle = function(filmsList, title) {
     return result;
 };
 
+Cinema.prototype.filterFilmsByGenre = function(filmsList, genre) {
+    const result = filmsList.filter((film) => {
+        return (film.genre === genre);
+    });
+    return result;
+};
+
+Cinema.prototype.checkReleaseYear = function(filmsList, year) {
+    const result = filmsList.filter((film) => {
+        return (film.year === year);
+    });
+    return result;
+};
+
+Cinema.prototype.checkFilmLength = function(filmsList, length) {
+    const result = filmsList.every((film) => {
+        return (film.length > length);
+    });
+    return result;
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -40,11 +40,30 @@ describe('Cinema', function() {
         assert.deepStrictEqual(actual, [dunkirk]);
     });
 
+    it('should be able to filter films by genre', function() {
+        const actual = cinema.filterFilmsByGenre(cinema.films, 'drama');
+        assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+    });
 
-    it('should be able to filter films by genre');
-    it('should be able to check whether there are some films from a particular year');
-    it('should be able to check whether there are no films from a particular year');
-    it('should be able to check whether all films are over a particular length');
-    it('should be able to calculate total running time of all films');
+    it('should be able to check whether there are some films from a particular year', function() {
+        const actual = cinema.checkReleaseYear(cinema.films, 2017);
+        assert.deepStrictEqual(actual, [bladeRunner, dunkirk, trainspotting]);
+    });
+
+    it('should be able to check whether there are no films from a particular year', function() {
+        const actual = cinema.checkReleaseYear(cinema.films, 2019);
+        assert.deepStrictEqual(actual, []);
+    });
+
+    it('should be able to check whether all films are over a particular length', function() {
+        const actual = cinema.checkFilmLength(cinema.films, 90);
+        assert.strictEqual(actual, true);
+
+    });
+    it('should be able to calculate total running time of all films', function() {
+        const actual = cinema.totalRunTime(cinema.films);
+        assert.strict(actual, 622)
+    });
+
 
 });
