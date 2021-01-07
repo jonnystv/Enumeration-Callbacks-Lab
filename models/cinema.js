f432618d64055b7522b1f474dfda7a6defa81d2d
Ewan Brain <ewan.brain@gmail.com>
2021-01-07 14:33:53 +0000

    first test passing

--- models/cinema.js
+++ models/cinema.js
@@ -1,5 +1,12 @@
-const Cinema = function (films) {
-  this.films = films;
+const Cinema = function(films) {
+    this.films = films;
+};
+
+Cinema.prototype.getListOfTitles = function(filmsList) {
+    const result = filmsList.map((film) => {
+        return film.title;
+    });
+    return result;
 };
 
 module.exports = Cinema;
--- specs/film_spec.js
+++ specs/film_spec.js
@@ -9,22 +9,22 @@ describe('Film', function () {
     moonlight = new Film('Moonlight', 'drama', 2016, 111);
   });
 
-  it('should have a title', function () {
+  xit('should have a title', function () {
     const actual = moonlight.title;
     assert.strictEqual(actual, 'Moonlight');
   });
 
-  it('should have a genre', function () {
+  xit('should have a genre', function () {
     const actual = moonlight.genre;
     assert.strictEqual(actual, 'drama');
   });
 
-  it('should have a year', function () {
+  xit('should have a year', function () {
     const actual = moonlight.year;
     assert.strictEqual(actual, 2016);
   });
 
-  it('should have a length', function () {
+  xit('should have a length', function () {
     const actual = moonlight.length;
     assert.strictEqual(actual, 111);
   });
